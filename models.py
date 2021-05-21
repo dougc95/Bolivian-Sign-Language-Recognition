eea48084de453f567a3dac6cf34fbe69d863b3f1
Douglas Rojas <dougcr95@gmail.com>
2021-05-21 16:54:59 -0400

    w10 fix reshaped (528 inside as a list)

--- models.py
+++ models.py
@@ -7,17 +7,22 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, ConvLSTM2D, BatchNormalization
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
-import tensorflow.keras.preprocessing
 
 from sklearn import preprocessing
 from collections import deque
+from sklearn.model_selection import train_test_split
+
+DATADIR = 'C:/Users/DougC/PycharmProjects/Bolivian-Sign-Language-Recognition/labelsLSB'
+CATEGORY = ['Ayuda', 'Bolivia', 'Como', 'Dinero', 'Doctor', 'Donde', 'Explicacion', 'Guardar',
+            'Necesito', 'Quien Occidente', 'Saludos']
 
 
 def get_dataFolders(CATEGORY, DATADIR):
     N_TIME_STEPS = 272
-    N_FEATURES = [42][42]
+    N_FEATURES = 42
+    DIMENSION = 2
     step = 2
-    segments = []
+    segments = np.empty(shape=(2,N_FEATURES,N_TIME_STEPS))
     labels = []
     for category in CATEGORY:
         # print(f'La categoria actual: {category}')
@@ -38,38 +43,40 @@ def get_dataFolders(CATEGORY, DATADIR):
                 ang = df[:, i + 1].astype(int)  # trans matDist = np.transpose(matDist) matAng = np.transpose(matAng)
                 matDist.append([mag])
                 matAng.append([ang])
-            segments.append([[[matDist[0, :], matDist[1, :], matDist[2, :], matDist[3, :], matDist[4, :],
-                               matDist[5, :], matDist[6, :], matDist[7, :], matDist[8, :], matDist[9, :],
-                               matDist[10, :], matDist[11, :], matDist[12, :], matDist[13, :],
-                               matDist[14, :], matDist[15, :], matDist[16, :], matDist[17], matDist[18, :],
-                               matDist[19, :], matDist[20, :], matDist[21, :], matDist[22, :], matDist[23, :],
-                               matDist[24, :], matDist[25, :], matDist[26, :], matDist[27, :],
-                               matDist[28, :], matDist[29, :], matDist[30, :], matDist[31, :], matDist[32, :],
-                               matDist[33, :], matDist[34, :], matDist[35, :], mag[36, :], mag[37, :], mag[38, :],
-                               mag[39, :], mag[40, :], mag[41, :]]],
-                             [[matAng[0, :], matAng[1, :], matAng[2, :], matAng[3, :], matAng[4, :], matAng[5, :],
-                               matAng[6, :], matAng[7, :], matAng[8, :], matAng[9, :], matAng[10, :], matAng[11, :],
-                               matAng[12, :], matAng[13, :],
-                               matAng[14, :], matAng[15, :], matAng[16, :], matAng[17], matAng[18, :],
-                               matAng[19, :], matAng[20, :], matAng[21, :], matAng[22, :], matAng[23, :],
-                               matAng[24, :], matAng[25, :], matAng[26, :], matAng[27, :],
-                               matAng[28, :], matAng[29, :], matAng[30, :], matAng[31, :], matAng[32, :],
-                               matAng[33, :], matAng[34, :], matAng[35, :], matAng[36, :], matAng[37, :],
-                               matAng[38, :], matAng[39, :], matAng[40, :], matAng[41, :]]]])
+            matDist = np.transpose(matDist)
+            #print(f'shape matDst {matDist}')
+            matAng = np.transpose(matAng)
+            np.append(segments, ([matDist[0], matDist[1], matDist[2], matDist[3], matDist[4],
+                                  matDist[5], matDist[6], matDist[7], matDist[8], matDist[9],
+                                  matDist[10], matDist[11], matDist[12], matDist[13],
+                                  matDist[14], matDist[15], matDist[16], matDist[17], matDist[18],
+                                  matDist[19], matDist[20], matDist[21], matDist[22], matDist[23],
+                                  matDist[24], matDist[25], matDist[26], matDist[27],
+                                  matDist[28], matDist[29], matDist[30], matDist[31], matDist[32],
+                                  matDist[33], matDist[34], matDist[35], matDist[36], matDist[37], matDist[38],
+                                  matDist[39], matDist[40], matDist[41],
+                                  matAng[0], matAng[1], matAng[2], matAng[3], matAng[4], matAng[5],
+                                  matAng[6], matAng[7], matAng[8], matAng[9], matAng[10], matAng[11],
+                                  matAng[12], matAng[13],
+                                  matAng[14], matAng[15], matAng[16], matAng[17], matAng[18],
+                                  matAng[19], matAng[20], matAng[21], matAng[22], matAng[23],
+                                  matAng[24], matAng[25], matAng[26], matAng[27],
+                                  matAng[28], matAng[29], matAng[30], matAng[31], matAng[32],
+                                  matAng[33], matAng[34], matAng[35], matAng[36], matAng[37],
+                                  matAng[38], matAng[39], matAng[40], matAng[41]]))
             labels.append(category)
     labels = np.asarray(pd.get_dummies(labels), dtype=np.float32)
+    aux = np.array(segments)
+    print(aux.shape)
     return labels, segments
 
 
-DATADIR = 'C:/Users/DougC/PycharmProjects/Bolivian-Sign-Language-Recognition/labelsLSB'
-CATEGORY = ['Ayuda', 'Bolivia', 'Como', 'Dinero', 'Doctor', 'Donde', 'Explicacion', 'Guardar',
-            'Necesito', 'Quien Occidente', 'Saludos']
-
 labels, segments = get_dataFolders(CATEGORY, DATADIR)
 N_TIME_STEPS = 272
-N_FEATURES = [42][42]
-reshaped_segments = np.asarray(segments, dtype=np.int).reshape(-1, N_TIME_STEPS, N_FEATURES)
-print(np.array(segments).shape)
+N_FEATURES = 42 + 42
+
+# WEA
+train_x, test_y, train_y, test_y = train_test_split(segments, labels, test_size=0.2)
 
 # Model params
 EPOCHS = 10
--- test.py
+++ test.py
@@ -141,5 +141,5 @@ history = model.fit(
     train_x, train_y,
     batch_size=BATCH_SIZE,
     epochs=EPOCHS,
-    validation_data=(validation_x, validation_y),
+    #validation_data=(validation_x, validation_y),
     callbacks=[tensorboard, checkpoint])
