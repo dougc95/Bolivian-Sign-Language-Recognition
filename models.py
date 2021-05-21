764a0bd7481730b77bd4d6597356d503dde18268
Douglas Rojas <dougcr95@gmail.com>
2021-05-21 09:16:34 -0400

    w10 better csv 545454554

--- models.py
+++ models.py
@@ -15,14 +15,15 @@ def get_dataFolders(CATEGORY, DATADIR):
     N_TIME_STEPS = 50
     N_FEATURES = 1
 
-    # Get X and Y indepents
+    # Get X and Y indepents DONE
     # Transpose
-    # segments.append([xs, ys])
-    # labels.append(category)
+    # segments.append([xs, ys]) DONE
+    # labels.append(category) DONE
     # labels = np.asarray(pd.get_dummies(labels), dtype = np.float32)
 
-    step = 1
+    step = 2
     segments = []
+    aux = []
     labels = []
     for category in CATEGORY:
         # print(f'La categoria actual: {category}')
@@ -31,23 +32,36 @@ def get_dataFolders(CATEGORY, DATADIR):
             # print(f"El file a rep es: {name}")
             file = os.path.join(path, name)
             df = pd.read_csv(file).to_numpy()
-            for i in range(0, df.shape[1]-1, step):
+            count=0
+            for i in range(0, df.shape[1], step):
                 mag = df[:, i].astype(int)
                 ang = df[:, i + 1].astype(int)
                 label = category
                 segments.append([mag, ang])
-                labels.append(label)
+                count=count+1
+            aux.append(segments)
+            labels.append(label)
+            print(f"COUNT: {count}")
             print(len(segments))
+            print(aux)
+    labels = np.asarray(pd.get_dummies(labels), dtype = np.float32)
+    #print(labels)
     return labels, segments
 
 
 DATADIR = 'C:/Users/DougC/PycharmProjects/Bolivian-Sign-Language-Recognition/labelsLSB'
 CATEGORY = ['Ayuda', 'Bolivia', 'Como', 'Dinero', 'Doctor', 'Donde', 'Explicacion', 'Guardar',
-            'Necesito', 'Quien Occidente', 'Quien Oriente', 'Saludos']
+            'Necesito', 'Quien Occidente', 'Saludos']
 
-N_TIME_STEPS = 48*
-N_FEATURES = 42*2
+
 labels, segments = get_dataFolders(CATEGORY, DATADIR)
-reshaped_segments = np.asarray(segments, dtype=np.float32).reshape(-1, N_TIME_STEPS, N_FEATURES)
+N_TIME_STEPS = 48*11
+N_FEATURES = 42*2
+reshaped_segments = np.asarray(segments, dtype=np.int).reshape(-1, N_TIME_STEPS, N_FEATURES)
 
 print(np.array(segments).shape)
+
+# Model params
+EPOCHS = 10
+BATCH_SIZE = 64
+NAME = f"{SEQ_LEN}-SEQ-{FUTURE_PERIOD_PREDICT}-PRED-{int(time.time())}"
